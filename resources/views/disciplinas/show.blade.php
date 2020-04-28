27460414f5b42818645dfa77722159d4baeb15f4
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2020-04-28 15:37:39 -0300

    issue10 - exibição de turmas por disciplina

--- resources/views/disciplinas/show.blade.php
+++ resources/views/disciplinas/show.blade.php
@@ -1,4 +1,12 @@
 <h1>{{ $disciplina->titulo }}</h1>
 <p>{{ $disciplina->ementa }}</p>
 <br>
+
+@foreach ($disciplina->turmas as $turma)
+    Ministrante: {{ $turma->ministrante }}<br>
+    Inicio: {{ $turma->inicio }}
+    <br><br>
+@endforeach
+
+<br>
 <a href="/disciplinas/{{ $disciplina->id }}/turmas/create">Inserir Turma</a>
