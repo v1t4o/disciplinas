87eaedb1ceb587268805a5bfd76637c198919f33
Victor de Oliveira Marinho <vitor.oliveira.marinho@gmail.com>
2020-05-01 14:32:21 -0300

    issue13 - Usando Bootstrap

--- resources/views/disciplinas/edit.blade.php
+++ resources/views/disciplinas/edit.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.master')
 
+@section('content')
 <form method="POST" action="/disciplinas/{{ $disciplina->id }}">
     {{ csrf_field() }}
     {{ method_field('patch') }}
@@ -7,3 +8,4 @@
     Ementa: <textarea name="ementa"> {{ $disciplina->ementa }} </textarea>
     <button type="submit"> Salvar </button>
 </form>
+@endsection
--- resources/views/disciplinas/index.blade.php
+++ resources/views/disciplinas/index.blade.php
@@ -1,21 +1,31 @@
 @extends('layouts.master')
 
+@section('content')
 
-<ul>
-@foreach ($disciplinas as $disciplina)
-    <li>
-        <a href="/disciplinas/{{ $disciplina->id }}">{{ $disciplina->titulo }}</a><br>
-        <a href="/disciplinas/{{ $disciplina->id }}/edit">Editar</a>
-    </li>
-    <br>
-    <form method="POST" action="/disciplinas/{{ $disciplina->id }}">
-        {{ csrf_field() }} 
-        {{ method_field('delete') }}
-        <button type="submit">Apagar</button>
-    </form>
-
-@endforeach
-</ul>
 <p>
-    <a class="btn btn-primary" href="/disciplinas/create" role="button">Cadastrar Nova Disciplina</a>
+    <a class="btn btn-success" href="/disciplinas/create" role="button">Cadastrar Nova Disciplina</a>
 </p>
+<table class="table table-striped">
+    <thead>
+        <tr>
+            <th>Nome da Disciplina</th>
+            <th>Editar</th>
+            <th>Apagar</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach ($disciplinas as $disciplina)
+        <tr>
+            <td><a href="/disciplinas/{{ $disciplina->id }}">{{ $disciplina->titulo }}</a><br></td>
+            <td><a href="/disciplinas/{{ $disciplina->id }}/edit">Editar</a></td>
+            <td>
+                <form method="POST" action="/disciplinas/{{ $disciplina->id }}">
+                    {{ csrf_field() }} 
+                    {{ method_field('delete') }}
+                    <button type="submit">Apagar</button>
+                </form>
+            </td>
+
+    @endforeach
+</table>
+@endsection
